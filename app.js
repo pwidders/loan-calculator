af295c790f0874c1222540f99fd955ddf087bf6b
Pete Widders <pwidders@hotmail.com>
2019-02-27 19:43:46 -0800

    Added spinning loading gif before running calculations

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 // Listen for submit
-document.getElementById('loan-data').addEventListener('submit', calculateResults);
+document.getElementById('loan-data').addEventListener('submit', function(e) {
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+
+  // Show loader
+  document.getElementById('loading').style.display = 'block';
+  // display for 2 seconds before running calculate function
+  setTimeout(calculateResults, 2000);
+
+   e.preventDefault(); 
+});
 
 // Calculate Results
-function calculateResults(e){
+function calculateResults(){
   console.log('Calculating...');
   // DOM elements
-  const amount = document.getElementById('amount');
+  const amount = document.getElementById('amount'); 
   const interest = document.getElementById('interest');
   const years = document.getElementById('years');
   const monthlyPayment = document.getElementById('monthly-payment');
@@ -24,9 +34,38 @@ function calculateResults(e){
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
-  } else {
+
+    // Display results
+    document.getElementById('results').style.display = 'block';
+    // Hide loading spinner
+    document.getElementById('loading').style.display = 'none';
+  } else {  // error handling 
     showError('Please check your numbers');
   }
+}
 
-  e.preventDefault();
+// showError
+function showError(error) {
+  const errorDiv = document.createElement('div');
+
+  // Get elements
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+  // Add class to div
+  errorDiv.className = 'alert alert-danger';
+
+  // Create text node and append it to the div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Insert error above heading
+  card.insertBefore(errorDiv, heading); // insertBefore method allows you to move elements before another 
+
+  // Clear error after 3 seconds
+  setTimeout(clearError, 3000);
+
+  // clearError function
+  function clearError() {
+    document.querySelector('.alert').remove();
+  }
 }
